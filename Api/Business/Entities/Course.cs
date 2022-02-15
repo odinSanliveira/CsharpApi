75bf6f12f4d4560a4df2d62bab6bba01263486b8
Odin Oliveira <odin.sanliveira@gmail.com>
2022-02-15 10:16:12 -0300

    criação dos models para o banco de dados.

---
+++ Api/Business/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsharpApi.Business.Entities
+{
+    public class Course
+    {
+        public int Code { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int UserCode { get; set; }
+        public virtual User User { get; set; }
+    }
+}
