15fb002d182d2562e17810d6e0d9e5ba7fea1f56
Odin Oliveira <odin.sanliveira@gmail.com>
2022-02-17 11:49:20 -0300

    adicionando projeto mvc

---
+++ api.web.mvc/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using api.web.mvc.Models.Users;
+using Microsoft.AspNetCore.Mvc;
+namespace api.web.mvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount(CreateUserViewModelInput creatingUser) 
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModelInput userLogin)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ api.web.mvc/Models/Users/CreateUserViewModelInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.web.mvc.Models.Users
+{
+    public class CreateUserViewModelInput
+    {      
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
